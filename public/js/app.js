721d140a8dac0cecfbf3f1378658e12befbd7d48
Nwe Nwe Win Than <scm.nwenwewinthan@gmail.com>
2022-01-25 14:16:59 +0630

    update readme

--- public/js/app.js
+++ public/js/app.js
@@ -35337,8 +35337,8 @@ try {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\bullatinboard\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\bullatinboard\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\OJT\bullatinboard-KMT\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\OJT\bullatinboard-KMT\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
